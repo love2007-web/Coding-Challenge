78be5c63058cecb2b62131a7319ed1722161993b
love2007-web <adeoluwaloveowolabi@gmail.com>
2023-09-23 04:14:46 +0100

    Having issues fixing the mobile navbar view

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,20 @@ import { HelmetProvider } from 'react-helmet-async'
 import { ToastContainer, toast } from 'react-toastify'
 import "react-toastify/dist/ReactToastify.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <HelmetProvider>
-        <App />
-        <ToastContainer />
-    </HelmetProvider>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HelmetProvider>
+    <App />
+    <ToastContainer
+      position="bottom-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+  </HelmetProvider>
+);
