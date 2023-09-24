8814f42598da1f6c65c972698a460824749f5de3
love2007-web <adeoluwaloveowolabi@gmail.com>
2023-09-24 02:46:22 +0100

    added more stars

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -280,159 +280,236 @@ const Rules = () => {
 };
 
 const Attributes = () => {
-
-    return (
-      <div className="border-b-[1px] border-[#ffffff2e]">
-        <Container>
-          <div className="grid lg:grid-cols-2 gap-[53px] max-w-max mx-auto pt-[100px]">
-            <figure className="relative flex items-center">
+  return (
+    <div className="border-b-[1px] border-[#ffffff2e]">
+      <Container>
+        <div className="grid lg:grid-cols-2 gap-[53px] max-w-max mx-auto pt-[100px]">
+          <figure className="relative flex items-center">
+            <img
+              src="/icons/star-purple.svg"
+              alt="Star Purple"
+              className="absolute w-[13px] h-[17px] top-[-12px] left-[50%] lg:w-[30px] lg:h-[36px] translate-x-[-50%] lg:top-[80px] lg:left-[150px]"
+            />
+            <img
+              src="/icons/star-grey.svg"
+              alt="Star Grey"
+              className="absolute w-[12px] h-[14px] left-[55%] top-[50%] lg:w-[26px] lg:h-[32px] translate-y-[-50%]"
+            />
+            <img
+              src="/images/attributes.svg"
+              alt="Judging Criteria Key attributes"
+              className="relative z-2"
+            />
+            <img
+              src="/images/purple-lens-flare-left.svg"
+              alt="purple lens flare"
+              className="absolute w-[1037px] h-[948px] pointer-events-none top-[-150px] lg:top-[150px] left-[-100px] mix-blend-hard-light lg:block"
+            />
+          </figure>
+          <div className="flex items-center pt-[78px] pb-[140px] relative">
+            <img
+              src="/images/purple-lens-flare-right.svg"
+              alt="purple lens flare"
+              className="absolute w-[1037px] h-[948px] pointer-events-none top-[400px] right-[-200px] mix-blend-hard-light"
+            />
+            <div className="w-full max-w-[564px] mx-auto grid place-items-center lg:block text-center lg:text-left relative">
               <img
-                src="/images/attributes.svg"
-                alt="Judging Criteria Key attributes"
-                className="relative z-2"
+                src="/icons/star-white.svg"
+                alt="Star White"
+                className="absolute w-[10px] h-[12px] lg:w-[26px] lg:h-[32px] right-[5%] bottom-[20px] lg:right-0 lg:bottom-[-5px] lg:left-[-100px]"
               />
-              <img
-                src="/images/purple-lens-flare-left.svg"
-                alt="purple lens flare"
-                className="absolute animate-pulse w-[1037px] h-[948px] pointer-events-none top-[-150px] lg:top-[150px] left-[-100px] mix-blend-hard-light  lg:block"
-              />
-            </figure>
-            <div className="flex items-center pb-[78px] relative">
-              <img
-                src="/images/purple-lens-flare-right.svg"
-                alt="purple lens flare"
-                className="absolute animate-pulse w-[1037px] h-[948px] pointer-events-none top-[400px] right-[-200px] mix-blend-hard-light"
-              />
-              <div className="w-full max-w-[564px] mx-auto grid place-items-center lg:block text-center lg:text-left">
-                <header className="text-white text-[32px] font-clashdisplay font-bold relative ">
-                  <h1 className="relative z-2">Judging Criteria</h1>
-                  <h1 className="text-primary relative z-2">Key attributes</h1>
-                </header>
-                <div className="pt-8 relative z-2 flex flex-col gap-8">
-                  {attributesData.map((ad) => (
-                    <p key={ad.id} className="text-white">
-                      <span className="text-[#FF26B9] font-bold">
-                        {ad.title}
-                      </span>{" "}
-                      {ad.content}
-                    </p>
-                  ))}
-                  <Button className="mt-6 mx-auto lg:mx-0">Read More</Button>
-                </div>
+              <header className="text-white text-[32px] font-clashdisplay font-bold relative ">
+                <h1 className="relative z-2">Judging Criteria</h1>
+                <h1 className="text-primary relative z-2">Key attributes</h1>
+              </header>
+              <div className="pt-8 relative z-2 flex flex-col gap-8">
+                {attributesData.map((ad) => (
+                  <p key={ad.id} className="text-white">
+                    <span className="text-[#FF26B9] font-bold">{ad.title}</span>{" "}
+                    {ad.content}
+                  </p>
+                ))}
+                <Button className="mt-6 mx-auto lg:mx-0">Read More</Button>
               </div>
             </div>
           </div>
-        </Container>
-      </div>
-    );
-}
+        </div>
+      </Container>
+    </div>
+  );
+};
 
 const Faqs = () => {
-
-    return (
-        <div className="border-b-[1px] border-[#ffffff2e]">
-            <Container>
-                <div className="flex gap-[24px] pb-14 flex-col lg:flex-row">
-                    <div className="w-full lg:max-w-[427px] pt-24">
-                        <header className="text-white relative pb-14 grid place-items-center lg:block text-center lg:text-left">
-                            <h1 className="relative z-2 text-[32px] font-clashdisplay font-bold">Frequently Ask </h1>
-                            <h1 className="text-primary relative z-2 text-[32px] font-clashdisplay font-bold">Question</h1>
-                            <p className="pt-2 text-[14px]">We got answers to the questions that you might
-                                want to ask about getlinked Hackathon 1.0</p>
-                        </header>
-                        <div>
-                            {
-                                faqsData.map(faq => (
-                                    <Disclosure key={faq.id}>
-                                        {({ open }) => (
-                                            <div className="mb-5">
-                                                <Disclosure.Button className="flex w-full justify-between border-b-[1px] border-primary py-2 text-left">
-                                                    <span className="text-white text-[14px]">{faq.title}</span>
-                                                    <span className={`material-icons self-end ${open ? "rotate-180" : "-rotate-180"} transition text-primary`}>
-                                                        {open ? "remove" : "add"}
-                                                    </span>
-                                                </Disclosure.Button>
-                                                <Transition
-                                                    enter="transition duration-100 ease-out"
-                                                    enterFrom="transform scale-95 opacity-0"
-                                                    enterTo="transform scale-100 opacity-100"
-                                                    leave="transition duration-75 ease-out"
-                                                    leaveFrom="transform scale-100 opacity-100"
-                                                    leaveTo="transform scale-95 opacity-0"
-                                                >
-                                                    <Disclosure.Panel className="text-left px-2 py-2 text-[13px] text-white text-opacity-80">
-                                                        {faq.content}
-                                                    </Disclosure.Panel>
-                                                </Transition>
-
-                                            </div>
-                                        )}
-                                    </Disclosure>
-                                ))
-                            }
-                        </div>
+  return (
+    <div className="border-b-[1px] border-[#ffffff2e]">
+      <Container>
+        <div className="flex gap-[24px] pb-14 flex-col lg:flex-row">
+          <div className="w-full lg:max-w-[427px] pt-24">
+            <header className="text-white relative pb-14 grid place-items-center lg:block text-center lg:text-left relative">
+              <img
+                src="/icons/star-purple.svg"
+                alt="Star Purple"
+                className="absolute w-[18px] h-[22px] lg:w-[30px] lg:h-[36px] left-[5px] min-[600px]:left-[70px] top-0 lg:left-[-30px] lg:top-[-50px]"
+              />
+              <h1 className="relative z-2 text-[32px] font-clashdisplay font-bold">
+                Frequently Ask{" "}
+              </h1>
+              <h1 className="text-primary relative z-2 text-[32px] font-clashdisplay font-bold">
+                Question
+              </h1>
+              <p className="pt-2 text-[14px]">
+                We got answers to the questions that you might want to ask about
+                getlinked Hackathon 1.0
+              </p>
+            </header>
+            <div>
+              {faqsData.map((faq) => (
+                <Disclosure key={faq.id}>
+                  {({ open }) => (
+                    <div className="mb-5">
+                      <Disclosure.Button className="flex w-full justify-between border-b-[1px] border-primary py-2 text-left">
+                        <span className="text-white text-[14px]">
+                          {faq.title}
+                        </span>
+                        <span
+                          className={`material-icons self-end ${
+                            open ? "rotate-180" : "-rotate-180"
+                          } transition text-primary`}
+                        >
+                          {open ? "remove" : "add"}
+                        </span>
+                      </Disclosure.Button>
+                      <Transition
+                        enter="transition duration-100 ease-out"
+                        enterFrom="transform scale-95 opacity-0"
+                        enterTo="transform scale-100 opacity-100"
+                        leave="transition duration-75 ease-out"
+                        leaveFrom="transform scale-100 opacity-100"
+                        leaveTo="transform scale-95 opacity-0"
+                      >
+                        <Disclosure.Panel className="text-left px-2 py-2 text-[13px] text-white text-opacity-80">
+                          {faq.content}
+                        </Disclosure.Panel>
+                      </Transition>
                     </div>
-                    <figure className="flex-1">
-                        <img src="/images/faqs.svg" alt="Faqs" />
-                    </figure>
-                </div>
-            </Container>
+                  )}
+                </Disclosure>
+              ))}
+            </div>
+          </div>
+          <figure className="flex-1 pt-[160px] relative grid place-items-center">
+            <div className="absolute flex gap-12 min-[400px]:gap-20 lg:gap-28 left-0 min-[365px]:left-[10%] min-[600px]:left-[20%] top-[10px]">
+              <img
+                src="/icons/question.svg"
+                alt="Question mark"
+                className="mt-[100px] w-[30px] lg:w-auto"
+              />
+              <img
+                src="/icons/question.svg"
+                alt="Question mark"
+                className="lg:w-[80px]"
+              />
+              <img
+                src="/icons/question.svg"
+                alt="Question mark"
+                className="mt-[100px] w-[30px] lg:w-auto"
+              />
+            </div>
+            <img src="/images/faqs.svg" alt="Faqs" />
+            <img
+              src="/icons/star-white.svg"
+              alt="Star White"
+              className="absolute w-[16px] h-[20px] lg:w-[26px] lg:h-[32px] bottom-0 right-[30%]"
+            />
+            <img
+              src="/icons/star-grey.svg"
+              alt="Star Grey"
+              className="absolute w-[10px] h-[12px] lg:w-[26px] lg:h-[32px] bottom-[36%] left-[10%]"
+            />
+            <img
+              src="/icons/star-dark-purple.svg"
+              alt="Star Dark Purple"
+              className="absolute w-[10px] h-[12px] lg:w-[26px] lg:h-[32px] top-[10%] left-[50%]"
+            />
+            <img
+              src="/icons/star-dark-purple.svg"
+              alt="Star Dark Purple"
+              className="absolute w-[10px] h-[12px] lg:w-[26px] lg:h-[32px] top-[30%] left-[20%]"
+            />
+          </figure>
         </div>
-    )
-}
-
+      </Container>
+    </div>
+  );
+};
 const Timeline = () => {
 
     return (
-        <div className="border-b-[1px] border-[#ffffff2e]">
-            <Container className="py-20">
-                <header className="w-full max-w-[346px] mx-auto text-center text-white mb-20">
-                    <h1 className="text-[32px] font-clashdisplay font-bold mb-2">Timeline</h1>
-                    <p className="text-[14px]">Here is the breakdown of the time we anticipate
-                        using for the upcoming event.</p>
-                </header>
-                <section className="hidden sm:block">
-                    {
-                        timelineData.map(tld => (
-                            tld.direction === 'left' ?
-                                <TimelineLeft
-                                    key={tld.id}
-                                    title={tld.title}
-                                    content={tld.content}
-                                    date={tld.date}
-                                    index={tld.index}
-                                    start={tld?.start}
-                                    end={tld?.end}
-                                />
-                                :
-                                <TimelineRight
-                                    key={tld.id}
-                                    title={tld.title}
-                                    content={tld.content}
-                                    date={tld.date}
-                                    index={tld.index}
-                                    start={tld?.start}
-                                    end={tld?.end}
-                                />
-                        ))
-                    }
-                </section>
-                <section className="block sm:hidden">
-                    {
-                        timelineData.map(tld => (
-                            <TimelineMobile
-                                key={tld.id}
-                                title={tld.title}
-                                content={tld.content}
-                                date={tld.date}
-                                index={tld.index}
-                            />
-                        ))
-                    }
-                </section>
-            </Container>
-        </div>
-    )
+      <div className="border-b-[1px] border-[#ffffff2e]">
+        <Container className="py-20 relative">
+          <img
+            src="/icons/star-purple.svg"
+            alt="Star Purple"
+            className="absolute top-[250px] left-[30%] w-[14px] h-[16px] lg:w-[30px] lg:h-[36px]"
+          />
+          <img
+            src="/icons/star-white.svg"
+            alt="Star White"
+            className="absolute top-[50%] right-[15%] w-[10px] h-[12px] lg:w-[26px] lg:h-[32px]"
+          />
+          <img
+            src="/icons/star-grey.svg"
+            alt="Star Grey"
+            className="absolute bottom-[40px] lg:bottom-[110px] left-[20px] lg:left-[15%] w-[10px] h-[12px] lg:w-[26px] lg:h-[32px]"
+          />
+          <header className="w-full max-w-[346px] mx-auto text-center text-white mb-20">
+            <h1 className="text-[32px] font-clashdisplay font-bold mb-2">
+              Timeline
+            </h1>
+            <p className="text-[14px]">
+              Here is the breakdown of the time we anticipate using for the
+              upcoming event.
+            </p>
+          </header>
+          <section className="hidden sm:block">
+            {timelineData.map((tld) =>
+              tld.direction === "left" ? (
+                <TimelineLeft
+                  key={tld.id}
+                  title={tld.title}
+                  content={tld.content}
+                  date={tld.date}
+                  index={tld.index}
+                  start={tld?.start}
+                  end={tld?.end}
+                />
+              ) : (
+                <TimelineRight
+                  key={tld.id}
+                  title={tld.title}
+                  content={tld.content}
+                  date={tld.date}
+                  index={tld.index}
+                  start={tld?.start}
+                  end={tld?.end}
+                />
+              )
+            )}
+          </section>
+          <section className="block sm:hidden">
+            {timelineData.map((tld) => (
+              <TimelineMobile
+                key={tld.id}
+                title={tld.title}
+                content={tld.content}
+                date={tld.date}
+                index={tld.index}
+              />
+            ))}
+          </section>
+        </Container>
+      </div>
+    );
 }
 
 const Rewards = () => {
